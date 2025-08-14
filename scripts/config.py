3412713567c9b13092bc8b3952e114721b6957f4
Am-Egej <10amegej@gmail.com>
2025-08-14 14:02:34 +0100

    Create config.py

---
+++ scripts/config.py
@@ -0,0 +1,8 @@
+DB_CONFIG = {
+    'host': 'data-sandbox.c1tykfvfhpit.eu-west-2.rds.amazonaws.com',
+    'port': 5432,
+    'dbname': 'pagila',
+    'schema': 'main',
+    'user': 'your_username', # From noodle
+    'password': 'your_password' # From noodle
+}

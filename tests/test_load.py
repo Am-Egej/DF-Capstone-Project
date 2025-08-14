081e665c6a6491bc644a143483d734fac74a4d1f
Am-Egej <10amegej@gmail.com>
2025-08-14 14:18:17 +0100

    Update test_load.py

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,1 +1,8 @@
-#creating the test
+import pandas as pd
+from scripts.load import load_to_postgres
+
+def test_load_function_runs():
+    data = 'PostgresSQL Data'
+    df = pd.DataFrame(data) # Use real connection to pagila here once it is done - to be editted.
+   
+    assert isinstance(df, pd.DataFrame) #Error will occur if no PostgreSQL database exists.

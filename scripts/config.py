9270b3adf98c0e7017735218594005a9e1d93175
Am-Egej <10amegej@gmail.com>
2025-08-14 18:23:51 +0100

    set schema in config.py

--- scripts/config.py
+++ scripts/config.py
@@ -2,7 +2,7 @@ DB_CONFIG = {
     'host': 'data-sandbox.c1tykfvfhpit.eu-west-2.rds.amazonaws.com',
     'port': 5432,
     'dbname': 'pagila',
-    'schema': 'main',
     'user': 'your_username', # From noodle
-    'password': 'your_password' # From noodle
+    'password': 'your_password', # From noodle
+    'options': '-c search_path=mystic_manuscript'
 }

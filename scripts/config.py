40171b0b41fd38b8b4e08d0be6b0fb38db452358
Am-Egej <10amegej@gmail.com>
2025-08-20 19:59:05 +0100

    git ignore files

--- scripts/config.py
+++
@@ -1,8 +0,0 @@
-DB_CONFIG = {
-    'host': 'data-sandbox.c1tykfvfhpit.eu-west-2.rds.amazonaws.com',
-    'port': 5432,
-    'dbname': 'pagila',
-    'user': 'your_username', # From noodle
-    'password': 'your_password', # From noodle
-    'options': '-c search_path=mystic_manuscript'
-}
